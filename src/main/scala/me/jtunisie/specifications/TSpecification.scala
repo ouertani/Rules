5b0a59579d9492f09592d9bc5eda458976bef21e
slim <ouertani@gmail.com>
2010-11-01 11:13:51 +0100

    use case lass

--- src/main/scala/me/jtunisie/specifications/TSpecification.scala
+++ src/main/scala/me/jtunisie/specifications/TSpecification.scala
@@ -12,36 +12,36 @@ trait TSpecification [T]{
   def |  ( specification : TSpecification[T]):TSpecification[T]
   def &  (specification :TSpecification[T] ):TSpecification[T]
 }
-abstract class ASpecification[T] extends  TSpecification [T]{
+
+ abstract class ASpecification[T] extends  TSpecification [T]{
   
   def   ->(candidate :T ):Boolean
-  def   ||( s : TSpecification[T]):TSpecification[T] = new OrSpecification(this,s)
-  def   &&(s :TSpecification[T] ):TSpecification[T] = new AndSpecification(this,s)
-  def   unary_!():TSpecification[T] = new NotSpecification(this)
+  def   ||( s : TSpecification[T]):TSpecification[T] =  OrSpecification(this,s)
+  def   &&(s :TSpecification[T] ):TSpecification[T] =  AndSpecification(this,s)
+  def   unary_!():TSpecification[T] =  NotSpecification(this)
 
-  // Recurssive Addon
+  // Recurssive Add-0ns
   def   | ( s : TSpecification[T]):TSpecification[T] = new ROrSpecification(this,s)
   def   &(s :TSpecification[T] ):TSpecification[T] = new RAndSpecification(this,s)
  
 }
 
-class AndSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
-  override def   ->(candidate :T ):Boolean= (true /: s) (_ -> candidate  &&  _ -> candidate)
+
+case class AndSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
+  override def   -> (candidate :T ):Boolean= (true /: s) (_ -> candidate  &&  _ -> candidate)
 
 }
-class OrSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
+case class OrSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
   override def   ->(candidate :T ):Boolean= (false /: s) (_ -> candidate  ||  _ -> candidate)
 }
-class NotSpecification[T]( s: TSpecification[T] ) extends ASpecification[T]{
+case class NotSpecification[T]( s: TSpecification[T] ) extends ASpecification[T]{
   override def   ->(candidate :T )=  ! (s -> candidate )
 }
 
 
-class RAndSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
-  
+case class RAndSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
   override def   ->(candidate :T ):Boolean= (s :\  true) (_ -> candidate  &&  _ -> candidate)
-
 }
-class ROrSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
+case class ROrSpecification[T]( s: TSpecification[T]* ) extends ASpecification[T]{
   override def   ->(candidate :T ):Boolean= (s :\ false) (_ -> candidate  ||  _ -> candidate)
 }
